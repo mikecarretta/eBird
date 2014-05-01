b1e726cf874c0b92950aaf5b8993746ee7d2097a
Mike Carretta <mikecarretta@gmail.com>
2014-04-30 20:38:48 -0400

    Added Search, Bootstrap Typeahead Directive

--- app/js/app.js
+++ app/js/app.js
@@ -4,14 +4,22 @@
 // Declare app level module which depends on filters, and services
 angular.module('ebird', [
   'ngRoute',
+  'ui.bootstrap',
   'ebird.services',
   'ebird.controllers'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.
-  when('/location', {templateUrl: 'app/partials/location.html', controller: 'LocCtrl'}).
-  when('/location/:locID', {templateUrl: 'app/partials/location-detail.html', controller: 'LocDetailCtrl'}).
-  when('/stratford', {templateUrl: 'app/partials/stratford.html', controller: 'LocationCtrl'}).
-  when('/about', {templateUrl: 'app/partials/about.html'}).
-  otherwise({redirectTo: '/location'});
+config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider) {
+  $routeProvider.when('/location', {templateUrl: 'app/partials/location.html', controller: 'SearchController'});
+  $routeProvider.when('/location/:locID', {templateUrl: 'app/partials/location-detail.html', controller: 'LocationController'});
+  $routeProvider.when('/about', {templateUrl: 'app/partials/about.html'});
+  $routeProvider.otherwise({redirectTo: '/location'});
+
+    // SPINNER.gif - the Factory is above all the ajax calls: myHttpInterceptor
+  // http://fdietz.github.io/recipes-with-angular-js/common-user-interface-patterns/displaying-a-loading-spinner.html
+  $httpProvider.responseInterceptors.push('myHttpInterceptor');
+    var spinnerFunction = function spinnerFunction(data, headersGetter) {
+      $("#spinner").show();
+      return data;
+    };
+  $httpProvider.defaults.transformRequest.push(spinnerFunction);
 }]);
--- app/js/services.js
+++ app/js/services.js
@@ -1,51 +1,46 @@
 'use strict';
 
 /* Services */
-angular.module('ebird.services', []).
-  factory('ebird', function($http) {
-    var ebirdAPI = {};
+angular.module('ebird.services', [])
+  .factory('getSearchLoc', ['$http', function($http) {
+    return {
+      eBirdApi: function(lat, lng) {
+        var eBirdRootURL = 'http://ebird.org/ws1.1/data/obs/geo/recent';
+        // Recent eBird Observations Made Nearby a Location
+        var eBirdDist = '30';
+        var eBirdDays = '30';
+        var eBirdMaxResults = 50;
 
-    ebirdAPI.getBirdJson = function() {
-      return $http({
-        url: 'app/js/data.json'
-      });
+        var eBirdGeoRecentURL = eBirdRootURL + '?lng=' + lng
+          + '&lat=' + lat + '&dist=' + eBirdDist + '&maxResults='
+          + eBirdMaxResults + '&locale=en_US' + '&fmt=json';
+        console.log(eBirdGeoRecentURL);
+
+        return $http({
+          url: eBirdGeoRecentURL
+        });
+      }
     }
+  }])
+  .factory('getLocation', ['$http', function($http) {
+    return {
+      eBirdApi: function(id) {
+        // http://ebird.org/ws1.1/data/obs/loc/recent?r=L99381&r=L104031&back=5&maxResults=500&detail=simple&locale=en_US&fmt=json&includeProvisional=true
+        var eBirdRootURL = 'http://ebird.org/ws1.1/data/obs/loc/recent?r=';
+        var max = 50;
+        var detail = 'full'; // simple, full
 
-    ebirdAPI.getBirdLoc = function() {
-      var eBirdRootURL = 'http://ebird.org/ws1.1/data/';
-      // Recent eBird Observations Made Nearby a Location
-      var eBirdDataURL = 'obs/geo/recent';
-      var eBirdLng = '-73.27';
-      var eBirdLat = '41.17';
-      var eBirdDist = '50';
-      var eBirdDays = '30';
-      var eBirdMaxResults = 30;
+        var apiUrl = eBirdRootURL + id + '&maxResults=' + max + '&detail=' + detail + '&fmt=json';
 
-      var eBirdGeoRecentURL = eBirdRootURL + eBirdDataURL + '?lng=' + eBirdLng
-        + '&lat=' + eBirdLat + '&dist=' + eBirdDist + '&maxResults='
-        + eBirdMaxResults + '&locale=en_US' + '&fmt=json';
+        console.log(apiUrl);
 
-      return $http({
-        url: eBirdGeoRecentURL
-      });
+        return $http({
+          url: apiUrl
+        });
+      }
     }
-
-    ebirdAPI.getBirdLocDetail = function(id) {
-      // http://ebird.org/ws1.1/data/obs/loc/recent?r=L99381&r=L104031&back=5&maxResults=500&detail=simple&locale=en_US&fmt=json&includeProvisional=true
-      var eBirdRootURL = 'http://ebird.org/ws1.1/data/obs/loc/recent?r=';
-      var max = 30;
-      var detail = 'full'; // simple, full
-
-      var apiUrl = eBirdRootURL + id + '&maxResults=' + max + '&detail=' + detail + '&fmt=json';
-
-      return $http({
-        url: apiUrl
-      });
-    }
-
-    return ebirdAPI;
-  }).
-  factory('myHttpInterceptor', function ($q, $window) {
+  }])
+  .factory('myHttpInterceptor', function ($q, $window) {
     return function (promise) {
       return promise.then(function (response) {
         $("#spinner").hide();
